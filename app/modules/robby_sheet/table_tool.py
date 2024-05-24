424a25564eebd68648c6b28ba0f97fde5d3a3c4c
balaji1732000 <balajisampath777@gmail.com>
2024-05-24 08:00:56 +0530

    changes01

--- app/modules/robby_sheet/table_tool.py
+++ app/modules/robby_sheet/table_tool.py
@@ -4,30 +4,29 @@ import os
 from io import StringIO, BytesIO
 import matplotlib.pyplot as plt
 import streamlit as st
+
 from langchain_community.llms import Replicate
 from langchain.callbacks import get_openai_callback
+from pandasai.callbacks import BaseCallback
 from streamlit_chat import message
 import replicate
+
 from pandasai import SmartDataframe
-from pandasai.callbacks import BaseCallback
 from dotenv import load_dotenv
 from pandasai.responses.response_parser import ResponseParser
-import pandas as pd
-from langchain.chains import LLMChain
-from langchain_community.llms import Replicate
-from langchain_core.prompts import PromptTemplate
-
 
 load_dotenv()
-
 # The REPLICATE_API_TOKEN will now be available in the environment variables
 os.environ["REPLICATE_API_TOKEN"] = os.getenv("REPLICATE_API_TOKEN")
 
 
 class PandasAgent:
+
     @staticmethod
     def count_tokens_agent(agent, query):
-        """Count the tokens used by the CSV Agent"""
+        """
+        Count the tokens used by the CSV Agent
+        """
         with get_openai_callback() as cb:
             result = agent(query)
             st.write(f"Spent a total of {cb.total_tokens} tokens")
@@ -35,6 +34,14 @@ class PandasAgent:
 
     def __init__(self):
         pass
+
+    class StreamlitCallback(BaseCallback):
+        def __init__(self, container) -> None:
+            """Initialize callback handler."""
+            self.container = container
+
+        def on_code(self, response: str):
+            self.container.code(response)
 
     class StreamlitResponse(ResponseParser):
         def __init__(self, context) -> None:
@@ -48,83 +55,45 @@ class PandasAgent:
             st.image(result["value"])
             return
 
-        def format_number(self, result):
-            st.write(result["value"])
-            return
-
-        def format_string(self, result):
-            st.write(result["value"])
-            return
-
         def format_other(self, result):
             st.write(result["value"])
             return
 
-        def handle_response(self, result):
-            if isinstance(result, dict) and "value" in result:
-                value = result["value"]
-                if isinstance(value, pd.DataFrame):
-                    self.format_dataframe(result)
-                elif isinstance(value, BytesIO):
-                    self.format_plot(result)
-                elif isinstance(value, (int, float)):
-                    self.format_number(result)
-                elif isinstance(value, str):
-                    self.format_string(result)
-                else:
-                    self.format_other(result)
-            else:
-                st.write("Unsupported result format")
-                st.write(result)
-            return
-
-    class StreamlitCallback(BaseCallback):
-        def __init__(self, container) -> None:
-            """Initialize callback handler."""
-            self.container = container
-
-        def on_code(self, response: str):
-            self.container.code(response)
-
     def get_agent_response(self, uploaded_file_content, query):
-        # llm = Replicate(
-        #     model="snowflake/snowflake-arctic-instruct",
-        #     model_kwargs={"temperature": 0.75, "max_length": 500, "top_p": 1},
-        # )
-
         llm = Replicate(
-            model="meta/meta-llama-3-8b-instruct",
+            model="snowflake/snowflake-arctic-instruct",
             model_kwargs={"temperature": 0.75, "max_length": 500, "top_p": 1},
         )
+        # llm = replicate(
+        #     model="snowflake/snowflake-arctic-instruct",
+        # )
         container = st.container()
+        # Using a dictionary for config instead of a set
         pandas_ai = SmartDataframe(
             uploaded_file_content,
             config={
                 "llm": llm,
-                "response_parser": self.StreamlitResponse,  # Correctly pass the class, not an instance
+                "response_parser": self.StreamlitResponse,
                 "callback": self.StreamlitCallback(container),
             },
         )
 
         old_stdout = sys.stdout
         sys.stdout = captured_output = StringIO()
-        try:
-            print("Running pandas_ai with query:", query)
-            response = pandas_ai.chat(query)  # Execute the query with pandas_ai
-            print("Response from pandas_ai:", response)
-            print("Response type:", type(response))
-            self.StreamlitResponse(self).handle_response({"value": response})
-            fig = plt.gcf()
-            if fig.get_axes():
-                fig.set_size_inches(12, 6)
-                plt.tight_layout()
-                buf = BytesIO()
-                fig.savefig(buf, format="png")
-                buf.seek(0)
-                st.image(buf, caption="Generated Plot")
-        except Exception as e:
-            print("Error during pandas_ai.run execution:", e)
-            response = None
+
+        response = pandas_ai.chat(query)
+        fig = plt.gcf()
+        if fig.get_axes():
+            # Adjust the figure size
+            fig.set_size_inches(12, 6)
+
+            # Adjust the layout tightness
+            plt.tight_layout()
+            buf = BytesIO()
+            fig.savefig(buf, format="png")
+            buf.seek(0)
+            st.image(buf, caption="Generated Plot")
+
         sys.stdout = old_stdout
         return response, captured_output
 
@@ -148,25 +117,3 @@ class PandasAgent:
                 message(message_text, is_user=True, key=f"{i}_user")
             else:
                 message(message_text, key=f"{i}")
-
-
-# # Initialize chat history if not already done
-# if "chat_history" not in st.session_state:
-#     st.session_state.chat_history = []
-
-# # File upload section
-# uploaded_file = st.file_uploader("Choose a file")
-# if uploaded_file is not None:
-#     uploaded_file_content = pd.read_csv(uploaded_file)
-
-#     # Query input section
-#     query = st.text_input("Ask DataNexus")
-#     if st.button("Submit"):
-#         agent = PandasAgent()
-#         response, captured_output = agent.get_agent_response(
-#             uploaded_file_content, query
-#         )
-#         cleaned_thoughts = agent.process_agent_thoughts(captured_output)
-#         agent.display_agent_thoughts(cleaned_thoughts)
-#         agent.update_chat_history(query, response)
-#         agent.display_chat_history()
